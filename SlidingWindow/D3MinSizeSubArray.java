4778967e02123fb194895317a39b0700db223276
Minal Kyada <minalkyada@Minals-MacBook-Pro.local>
2023-11-08 22:28:11 -0600

    D3 of Week 1 array

---
+++ SlidingWindow/D3MinSizeSubArray.java
@@ -0,0 +1,22 @@
+package SlidingWindow;
+
+// https://leetcode.com/problems/minimum-size-subarray-sum/submissions/1094940942/
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        
+        int left=0;
+        int right=0;
+        int sum=0;
+        int res=Integer.MAX_VALUE;
+
+        for(right=0;right<nums.length;right++){
+            sum+=nums[right];
+            while(sum>=target){
+                res=Math.min(res,right-left+1);
+                sum-=nums[left++];
+            }
+        }
+        return res==Integer.MAX_VALUE ? 0:res;
+    }
+}
