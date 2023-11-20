d6282d4340937874a32b95b6a649598a0c31791e
Minal Kyada <minalkyada@Minals-MacBook-Pro.local>
2023-11-19 23:09:21 -0600

    100 day

--- 100DayChallenge/D21ValidParenthesis.java
+++ 100DayChallenge/D21ValidParenthesis.java
@@ -1,1 +1,19 @@
 // https://leetcode.com/problems/valid-parentheses/
+
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') st.push(')');
+            else if (c == '{') st.push('}');
+            else if (c == '[') st.push(']');
+            else {
+                if(st.isEmpty() || c!=st.pop()) return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
---
+++ 10DayStreak/D1ReductionOperation.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal/?envType=daily-question&envId=2023-11-19
+
+import java.util.*;
+
+class Solution {
+    public int reductionOperations(int[] nums) {
+        int totalOperations = 0;
+        Arrays.sort(nums);
+        for (int i=nums.length-1; i>0; i--) {
+            if (nums[i] != nums[i-1])
+                totalOperations+= (nums.length-i);
+        }
+        return totalOperations;
+    }
+}
